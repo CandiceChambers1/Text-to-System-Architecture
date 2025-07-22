610367fd3f97270e2cbcd2236fc21b8eeb1f3080
CandiceChambers1 <chambersc2017@my.fit.edu>
2025-07-22 16:12:55 -0400

    edited file path

--- Java/src/main/CreateAADL.java
+++ Java/src/main/CreateAADL.java
@@ -83,7 +83,7 @@ public class CreateAADL {
 
 //        System.out.println(output);
         try {
-            String filePath = "./src/gen/aadl/aadl/" + filename + "_Automated.aadl";
+            String filePath = "./src/gen/aadl/models/" + filename + "_Automated.aadl";
 
             // Ensure the directory exists
             File file = new File(filePath);
